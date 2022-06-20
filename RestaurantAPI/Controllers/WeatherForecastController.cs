9fe1db9a7b5219212d5a7fdd237607026f3c1b63
mgremek <k.pawlak666@gmail.com>
2022-06-20 22:18:54 +0200

    Wydzielenie WeatherForecastService + rejestracja interfejsu IWeatherForecastService  w Startup

--- RestaurantAPI/Controllers/WeatherForecastController.cs
+++ RestaurantAPI/Controllers/WeatherForecastController.cs
@@ -11,29 +11,20 @@ namespace RestaurantAPI.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
+        private IWeatherForecastService _forecastService;
+       
         private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IWeatherForecastService forecastService)
         {
             _logger = logger;
+            _forecastService = forecastService;
         }
 
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return _forecastService.Get();
         }
     }
 }
