0300b4f7f4a87f9536ca5da7d026a2da99b589ce
mgremek <k.pawlak666@gmail.com>
2022-07-19 18:18:30 +0200

    Dodanie resturantKontrolera

--- RestaurantAPI/Controllers/RestaurantController.cs
+++ RestaurantAPI/Controllers/RestaurantController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RestaurantAPI.Entities;
+using RestaurantAPI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,15 +18,17 @@ namespace RestaurantAPI.Controllers
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<Restaurant>> GetAll()
+        public ActionResult<IEnumerable<RestaurantDTO>> GetAll()
         {
             return Ok(_dbContext.Restaurants.ToList()) ;
         }
 
         [HttpGet("{restaurantId}")]
-        public ActionResult<Restaurant> Get([FromRoute] int restaurantId)
+        public ActionResult<RestaurantDTO> Get([FromRoute] int restaurantId)
         {
             var restaurant = _dbContext.Restaurants.FirstOrDefault(x => x.Id == restaurantId);
+
+
 
             if (restaurant != null)
                 return Ok(restaurant);
