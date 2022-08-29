76397ccd55a6e713d95f3d39a6800bc4ecf75b1f
mgremek <k.pawlak666@gmail.com>
2022-08-29 19:32:34 +0200

    Dodanie middleware do logowania errorów

---
+++ RestaurantAPI/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Middleware
+{
+    public class ErrorHandlingMiddleware : IMiddleware
+    {
+        private readonly ILogger<ErrorHandlingMiddleware> _logger;
+
+        public ErrorHandlingMiddleware(ILogger<ErrorHandlingMiddleware> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next.Invoke(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+
+                context.Response.StatusCode = 500;
+                await context.Response.WriteAsync("Something went wrong");
+            }
+        }
+    }
+}
