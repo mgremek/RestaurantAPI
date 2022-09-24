f9b1e036d1eceb07c5073b2fbd72fdb8ddb21a9f
mgremek <k.pawlak666@gmail.com>
2022-09-24 10:38:45 +0200

    Dodanie podstawowej akcji rejestracji usera

---
+++ RestaurantAPI/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantAPI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Controllers
+{
+    [Route("api/account")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+        [HttpPost("register")] 
+        public ActionResult RegisterUser([FromBody] RegisterUserDTO registerUserDTO)
+        {
+            _accountService.RegisterUser(registerUserDTO);
+            return Ok();
+
+        }
+    }
+}
---
+++ RestaurantAPI/Services/AccountService.cs
@@ -0,0 +1,35 @@
+﻿using RestaurantAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Services
+{
+    public interface IAccountService
+    {
+        void RegisterUser(RegisterUserDTO dto);
+    }
+    public class AccountService : IAccountService
+    {
+        private readonly RestaurantDbContext _dbContext;
+
+        public AccountService(RestaurantDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void RegisterUser(RegisterUserDTO dto)
+        {
+            var newUser = new User()
+            {
+                Email = dto.Email,
+                DateOfBirth = dto.DateOfBirth,
+                Nationality = dto.Nationality,
+                RoleId = dto.RoleId
+            };
+
+            _dbContext.Users.Add(newUser);
+            _dbContext.SaveChanges();
+        }
+    }
+}
