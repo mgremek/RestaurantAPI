ab515e3bb914931c011b63f5102099eb0166ad89
mgremek <k.pawlak666@gmail.com>
2022-11-06 12:34:27 +0100

    Dodanie controllera do pobierania plików z serwera

---
+++ RestaurantAPI/Controllers/FileController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Controllers
+{
+    [Route("file")]
+    //[Authorize]
+    public class FileController : ControllerBase
+    {
+        public ActionResult GetFile([FromQuery]string fileName)
+        {
+            var filePath = $"{Directory.GetCurrentDirectory()}/PrivateFiles/{fileName}";
+
+            if (!System.IO.File.Exists(filePath))
+                return NotFound();
+
+            var contentProvider = new FileExtensionContentTypeProvider();
+            contentProvider.TryGetContentType(fileName, out string contentType);
+
+            return File(System.IO.File.ReadAllBytes(filePath), contentType, fileName);
+        }
+    }
+}
