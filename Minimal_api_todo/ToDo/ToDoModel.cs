11b7dbc7e25da5e944a93e33e338985918485166
mgremek <k.pawlak666@gmail.com>
2023-01-06 16:56:06 +0100

    Utworzenie projektu MinimalApi

---
+++ Minimal_api_todo/ToDo/ToDoModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDo.MinimalApi
+{
+    public class ToDoModel
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Value { get; set; }
+        public bool IsCompleted { get; set; }
+            
+    }
+}
---
+++ Minimal_api_todo/ToDo/ToDoService.cs
@@ -0,0 +1,46 @@
+﻿namespace ToDo.MinimalApi
+{
+    public interface IToDoService
+    {
+        void Create(ToDoModel ToDoModel);
+        void Delete(Guid id);
+        List<ToDoModel> GetAll();
+        ToDoModel GetById(Guid id);
+        void Update(ToDoModel ToDoModel);
+    }
+
+    public class ToDoService : IToDoService
+    {
+        private readonly Dictionary<Guid, ToDoModel> _ToDoModels = new();
+        public ToDoService()
+        {
+            var sampleToDoModel = new ToDoModel { Value = "Learn MinimalApi" };
+            _ToDoModels[sampleToDoModel.Id] = sampleToDoModel;
+        }
+
+        public ToDoModel GetById(Guid id) => _ToDoModels.GetValueOrDefault(id);
+
+        public List<ToDoModel> GetAll() => _ToDoModels.Values.ToList();
+
+        public void Create(ToDoModel ToDoModel)
+        {
+            if (ToDoModel is null)
+                return;
+
+            _ToDoModels[ToDoModel.Id] = ToDoModel;
+        }
+
+        public void Update(ToDoModel ToDoModel)
+        {
+            if (ToDoModel is null)
+                return;
+
+            _ToDoModels[ToDoModel.Id] = ToDoModel;
+        }
+
+        public void Delete(Guid id)
+        {
+            _ToDoModels.Remove(id);
+        }
+    }
+}
