a017129595064be66c5627d543cffd0a2a92d6c4
mgremek <k.pawlak666@gmail.com>
2023-05-20 12:14:47 +0200

    InMemoryDBContext

--- RestaurantApi.IntegrationTests/RestaurantControllerTests.cs
+++ RestaurantApi.IntegrationTests/RestaurantControllerTests.cs
@@ -1,5 +1,8 @@
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RestaurantAPI.Entities;
 using System.Net;
 using Xunit;
 
@@ -12,7 +15,20 @@ namespace RestaurantApi.IntegrationTests
         public RestaurantControllerTests(WebApplicationFactory<Program> factory)
         {
             factory = new WebApplicationFactory<Program>();
-            _client = factory.CreateClient();
+            _client = factory
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        var dbContextOptions = services
+                            .SingleOrDefault(service => service.ServiceType == typeof(DbContextOptions<RestaurantDbContext>));
+
+                        services.Remove(dbContextOptions);
+
+                        services.AddDbContext<RestaurantDbContext>(options => options.UseInMemoryDatabase("ResturantDb"));
+                    });
+                })
+                .CreateClient();
         }
         [Theory]
         [InlineData("pageSize=5&pageNumber=1")]
@@ -24,18 +40,16 @@ namespace RestaurantApi.IntegrationTests
             var response = await _client.GetAsync($"/api/restaurant?{query}");
 
             response.StatusCode.Should().Be(HttpStatusCode.OK);
-
         }
+
         [Theory]
         [InlineData("pageSize=4&pageNumber=1")]
         [InlineData("pageSize=11&pageNumber=2")]
         public async Task GetAll_WithQueryParameters_ReturnsBadRequest(string query)
-        {
-           
+        {          
             var response = await _client.GetAsync($"/api/restaurant?{query}");
 
             response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
-
         }
     }
 }
