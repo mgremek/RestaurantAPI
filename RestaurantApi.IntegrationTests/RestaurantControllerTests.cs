07816c7a5e3a288798c4762184f66f0b1f51f3e5
mgremek <k.pawlak666@gmail.com>
2023-05-20 10:59:05 +0200

    wspoldzielony kontekst w testach

--- RestaurantApi.IntegrationTests/RestaurantControllerTests.cs
+++ RestaurantApi.IntegrationTests/RestaurantControllerTests.cs
@@ -1,21 +1,27 @@
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
 using System.Net;
+using Xunit;
 
 namespace RestaurantApi.IntegrationTests
 {
-    public class RestaurantControllerTests
+    public class RestaurantControllerTests :IClassFixture<WebApplicationFactory<Program>>
     {
+        private readonly HttpClient _client;
+        
+        public RestaurantControllerTests(WebApplicationFactory<Program> factory)
+        {
+            factory = new WebApplicationFactory<Program>();
+            _client = factory.CreateClient();
+        }
         [Theory]
         [InlineData("pageSize=5&pageNumber=1")]
         [InlineData("pageSize=15&pageNumber=2")]
         [InlineData("pageSize=10&pageNumber=3")]
         public async Task GetAll_WithQueryParameters_ReturnsOkResult(string query)
         {
-            var factory = new WebApplicationFactory<Program>();
-            var client = factory.CreateClient();
 
-            var response = await client.GetAsync($"/api/restaurant?{query}");
+            var response = await _client.GetAsync($"/api/restaurant?{query}");
 
             response.StatusCode.Should().Be(HttpStatusCode.OK);
 
@@ -25,10 +31,8 @@ namespace RestaurantApi.IntegrationTests
         [InlineData("pageSize=11&pageNumber=2")]
         public async Task GetAll_WithQueryParameters_ReturnsBadRequest(string query)
         {
-            var factory = new WebApplicationFactory<Program>();
-            var client = factory.CreateClient();
-
-            var response = await client.GetAsync($"/api/restaurant?{query}");
+           
+            var response = await _client.GetAsync($"/api/restaurant?{query}");
 
             response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
 
