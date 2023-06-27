532e2bd8b0f8aa6517f981f06f09342aaf2b3514
mgremek <k.pawlak666@gmail.com>
2023-06-27 22:05:31 +0200

    Dodanie testów restaurant walidatora

---
+++ RestaurantApi.IntegrationTests/RestaurantQueryValidationTests.cs
@@ -0,0 +1,100 @@
+﻿using FluentValidation.TestHelper;
+using RestaurantAPI.Entities;
+using RestaurantAPI.Models;
+using RestaurantAPI.Models.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantApi.IntegrationTests
+{
+    public  class RestaurantQueryValidationTests
+    {
+        public static IEnumerable<object[]> GetSampleValidData()
+        {
+            var list = new List<RestaurantQuery>()
+            {
+                new RestaurantQuery()
+                {
+                    PageNumber=1,
+                    PageSize=5,
+                    SortBy=nameof(Restaurant.Name)
+                },
+                new RestaurantQuery()
+                {
+                    PageNumber=8,
+                    PageSize=10,
+                    SortBy=nameof(Restaurant.Name)
+                },
+                new RestaurantQuery()
+                {
+                    PageNumber=1,
+                    PageSize=15,
+                    SortBy=nameof(Restaurant.Name)
+                },
+                new RestaurantQuery()
+                {
+                    PageNumber=12,
+                    PageSize=15,
+                    SortBy=nameof(Restaurant.Category)
+                },
+            };
+
+            return list.Select(x => new object[] { x });
+        }
+
+        public static IEnumerable<object[]> GetSampleInvalidData() 
+        {
+            var list = new List<RestaurantQuery>
+            {
+                new RestaurantQuery()
+                {
+                    PageNumber=1,
+                    PageSize=1,
+                },
+                new RestaurantQuery()
+                {
+                    PageNumber=0,
+                    PageSize=10,
+                },
+                new RestaurantQuery()
+                {
+                    PageNumber=1,
+                    PageSize=15,
+                    SortBy=nameof(Restaurant.Address)
+                },
+                new RestaurantQuery()
+                {
+                    PageNumber=-1,
+                    PageSize=11,
+                    SortBy=nameof(Restaurant.HasDelivery)
+                },
+            };
+
+            return list.Select(_ => new object[] { _ });
+        }
+        [Theory]
+        [MemberData(nameof(GetSampleValidData))]
+        public void Validate_ForCorrectModel_ReturnsSuccess(RestaurantQuery model)
+        {
+            var validator = new RestaurantQueryValidator();          
+
+            var result = validator.TestValidate(model);
+
+            result.ShouldNotHaveAnyValidationErrors(); 
+        }
+
+        [Theory]
+        [MemberData(nameof(GetSampleInvalidData))]
+        public void Validate_ForIncorrectModel_ReturnFailure(RestaurantQuery model)
+        {
+            var validator = new RestaurantQueryValidator();
+
+            var result = validator.TestValidate(model);
+
+            result.ShouldHaveAnyValidationError();
+        }
+    }
+}
